8628a2fcfc5e0ac2216e828d65ae03a00c635407
Dipak Bari <dipakbari4@gmail.com>
2021-11-07 20:32:04 +0530

    Initial Commit

---
+++ android/app/src/main/kotlin/com/dipak/paintr/paintr/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.dipak.paintr.paintr
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Paintr"),
+        ),
+        body: CustomPaint(
+          painter: ShapePainter(),
+          child: Container(),
+        ),
+      ),
+    );
+  }
+}
+
+class ShapePainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    var paint = Paint()
+      ..color = Colors.teal
+      ..strokeWidth = 5
+      ..strokeCap = StrokeCap.round;
+
+    var p1 = Offset(0, size.height / 2);
+    var p2 = Offset(size.width, size.height / 2);
+
+    canvas.drawLine(p1, p2, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return false;
+  }
+}
