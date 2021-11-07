5f223cc94ec3cc83066897107413c56a4bc96ca8
Dipak Bari <dipakbari4@gmail.com>
2021-11-07 20:36:36 +0530

    Line Painter

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'line.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,30 +16,10 @@ class MyApp extends StatelessWidget {
           title: const Text("Paintr"),
         ),
         body: CustomPaint(
-          painter: ShapePainter(),
+          painter: Line(),
           child: Container(),
         ),
       ),
     );
-  }
-}
-
-class ShapePainter extends CustomPainter {
-  @override
-  void paint(Canvas canvas, Size size) {
-    var paint = Paint()
-      ..color = Colors.teal
-      ..strokeWidth = 5
-      ..strokeCap = StrokeCap.round;
-
-    var p1 = Offset(0, size.height / 2);
-    var p2 = Offset(size.width, size.height / 2);
-
-    canvas.drawLine(p1, p2, paint);
-  }
-
-  @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) {
-    return false;
   }
 }
